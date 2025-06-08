41dc0b24cc01f2a8b6f479a161919b3c92e58c0b
Fabian Braun <fsbraun@gmx.de>
2025-06-08 19:45:49 +0200

    Fix ruff

--- djangocms_rest/views.py
+++ djangocms_rest/views.py
@@ -10,7 +10,6 @@ from rest_framework.response import Response
 
 from djangocms_rest.permissions import (
     CanViewPage,
-    CanViewPageContent,
     IsAllowedPublicLanguage,
 )
 from djangocms_rest.serializers.languages import LanguageSerializer
